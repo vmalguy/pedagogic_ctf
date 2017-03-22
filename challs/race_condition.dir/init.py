4da228810f9a9f9f1a6ff20e0377527e917dc904
Vincent Malguy <vincent.malguy@corp.ovh.com>
2017-03-22 17:22:17 +0100

    hack hashed format for all languages to be happy

--- challs/race_condition.dir/init.py
+++ challs/race_condition.dir/init.py
@@ -26,7 +26,9 @@ def init(path, randomize, file_challenge_name=None):
     cur.execute("""CREATE TABLE forbidden_ids (
 		user_id INTEGER NOT NULL UNIQUE)""")
     conn.commit()
-    hashed_randomize = "$2a$12$mOPugE7rXVJZfWksHDF06ukZg.Zp.DGLA7B1JpSPYfw94dDjHMEDS"
+    #salt =  "$2a$12$mOPugE7rXVJZfWksHDF06uLwT9RXL7cdWFWzuGD70RSPbnXpLKqQe"
+    hashed_randomize =  bcrypt.hashpw(randomize.encode("utf-8"), bcrypt.gensalt(8)).decode('utf-8')
+    hashed_randomize = hashed_randomize.replace("$2b$", "$2a$")
     cur.execute("INSERT INTO users(login, password) VALUES(?, ?)", [randomize, hashed_randomize])
     conn.commit()
     conn.close()
